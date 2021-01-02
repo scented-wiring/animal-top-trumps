5b7b448bb894ebbe71aec2a814e0859403492f75
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-02 19:02:36 +0000

    create App component, styling and tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import "../styles/App.css";
+import logo from "../dog.png";
 
 function App() {
-  return <div className="App">Animal Top Trumps</div>;
+  return (
+    <div className="App">
+      <div className="title">
+        <img id="logo" src={logo} alt="Logo" width="70px" height="70px" />
+        <h1>Animal Top Trumps</h1>
+      </div>
+      <div className="buttons">
+        <button type="button">Play</button>
+        <button type="button">Create Cards</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "../components/App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/animal top trumps/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders correctly", () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment).toMatchSnapshot();
+});
+
+test("renders the page title", () => {
+  const { getByText } = render(<App />);
+  expect(getByText("Animal Top Trumps")).toBeInTheDocument();
+});
+
+test("renders two buttons", () => {
+  const { getAllByRole } = render(<App />);
+  expect(getAllByRole("button")).toHaveLength(2);
 });
