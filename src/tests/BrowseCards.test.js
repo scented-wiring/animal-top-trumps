13ef55e9b7c97e6dadb1d26e892d1edb616a4fb9
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-10 13:31:25 +0000

    add tests for Card rendering in BrowseCards

--- src/tests/BrowseCards.test.js
+++ src/tests/BrowseCards.test.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import BrowseCards from "../components/BrowseCards";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import { fireEvent, getByText } from "@testing-library/react";
 
 const renderContainer = () => {
   render(
@@ -15,7 +16,7 @@ const renderContainer = () => {
 };
 
 const fakeResponse = [
-  { id: 1, name: "Cat" },
+  { id: 1, name: "Cat", aka: "Furry demon", alignment: "Lawful Evil" },
   { id: 2, name: "Dog" },
   { id: 3, name: "Elephant" },
 ];
@@ -80,5 +81,32 @@ describe("BrowseCards component", () => {
 
     expect(container.textContent).toContain("CatDogElephant");
     expect(options).toHaveLength(3);
+  });
+
+  test("renders a card with default text on load", async () => {
+    var mock = new MockAdapter(axios);
+    mock.onGet("http://localhost:3000/cards").reply(200, fakeResponse);
+
+    await act(async () => {
+      renderContainer();
+    });
+    expect(container.textContent).toContain(
+      "Choose a card from the left to view its stats"
+    );
+  });
+
+  test("shows stats of card from select menu when clicked", async () => {
+    var mock = new MockAdapter(axios);
+    mock.onGet("http://localhost:3000/cards").reply(200, fakeResponse);
+
+    await act(async () => {
+      renderContainer();
+    });
+
+    fireEvent.click(getByText(container, "Cat"));
+
+    expect(container.textContent).toContain(
+      'aka: "Furry demon" Alignment:Lawful Evil'
+    );
   });
 });
