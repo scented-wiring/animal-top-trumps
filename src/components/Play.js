5e831c6a4bf8eec8e19fd8912d7e59142a5a09bc
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-11 18:48:25 +0000

    Play component renders player & computer card, fix async logic that caused loading error

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,6 +1,8 @@
+import "../styles/Play.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Alert from "../components/Alert";
+import Card from "../components/Card";
 
 const Play = () => {
   const initialState = {
@@ -11,7 +13,9 @@ const Play = () => {
   };
 
   const [playerCards, setPlayerCards] = useState([]);
+  const [playerCard, setPlayerCard] = useState({});
   const [computerCards, setComputerCards] = useState([]);
+  const [computerCard, setComputerCard] = useState({});
   const [alert, setAlert] = useState(initialState.alert);
   const [load, setLoad] = useState([true]);
 
@@ -54,7 +58,11 @@ const Play = () => {
     setLoad(true);
     axios
       .get("http://localhost:3000/cards")
-      .then((response) => deal(shuffle(response.data)))
+      .then((response) => {
+        deal(shuffle(response.data));
+        setPlayerCard(playerCards[0]);
+        setComputerCard(computerCards[0]);
+      })
       .catch(() => {
         setAlert({
           message: "Could not connect to the server.",
@@ -64,15 +72,26 @@ const Play = () => {
     setLoad(false);
   }, [computerCards, playerCards]);
 
-  return (
-    <div className="Play">
-      <Alert
-        message={alert.message}
-        alertType={alert.alertType}
-        onAlertPress={handleAlertPress}
-      />
-    </div>
-  );
+  if (!load) {
+    return (
+      <div className="Play">
+        <div className="card-display">
+          <Card {...playerCard} /> <Card {...computerCard} />
+        </div>
+        <Alert
+          message={alert.message}
+          alertType={alert.alertType}
+          onAlertPress={handleAlertPress}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="Play">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
 };
 
 export default Play;
