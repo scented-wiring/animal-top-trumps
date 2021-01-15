10bee64223957f55308b5e03d07a25d397997113
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-15 17:45:55 +0000

    add link descriptions on hover, add home page image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,55 @@
 import "../styles/Home.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import pokerdogs from "../pokerdogs.png";
 
 const Home = () => {
+  const [hover, setHover] = useState("");
+
   return (
     <div className="home">
+      <img
+        id="pokerdogs"
+        src={pokerdogs}
+        alt="dogs playing poker"
+        width="550px"
+      />
       <div className="buttons">
         <Link to="/play">
-          <button type="button">Play</button>
+          <button
+            type="button"
+            onMouseOver={() => setHover("play")}
+            onMouseOut={() => setHover("")}
+          >
+            Play
+          </button>
         </Link>
         <Link to="/browsecards">
-          <button type="button">Browse Cards</button>
+          <button
+            type="button"
+            onMouseOver={() => setHover("browse")}
+            onMouseOut={() => setHover("")}
+          >
+            Browse Cards
+          </button>
         </Link>
         <Link to="/createcards">
-          <button type="button">Create Cards</button>
+          <button
+            type="button"
+            onMouseOver={() => setHover("create")}
+            onMouseOut={() => setHover("")}
+          >
+            Create Cards
+          </button>
         </Link>
+      </div>
+      <div className="description">
+        {hover === "play" &&
+          "Battle against a highly skilled computer using your Top Trumps deck."}
+        {hover === "browse" &&
+          "View the stats of (or delete) cards from your Top Trumps deck."}
+        {hover === "create" &&
+          "Add personalised cards to your Top Trumps deck."}
       </div>
     </div>
   );
