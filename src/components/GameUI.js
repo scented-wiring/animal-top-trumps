0e1058208b591f0656909b47feff33a3d50b57d3
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-17 13:23:57 +0000

    refactor GameUI

--- src/components/GameUI.js
+++ src/components/GameUI.js
@@ -13,114 +13,88 @@ const GameUI = ({
   tieCardsLength,
   noCards,
 }) => {
-  if (gameWinner) {
-    return (
-      <div className="game-ui">
-        <div className="status">{gameWinner} wins the game!</div>
-        <div className="message">Game over!</div>
+  let status, message;
+
+  if (noCards) {
+    //if no cards in deck
+    status = "No cards found!";
+    message = 'Go to "Create Cards" from the menu to add cards to your deck.';
+  } else if (gameWinner) {
+    //if game is over
+    status = `${gameWinner} wins the game!`;
+    message = "Game over!";
+  } else if (win === "Tie") {
+    //if action results in tie
+    status = `${win}!`;
+    message = `Top player and computer cards added to tie deck. ${tieCardsLength} tie cards up for grabs next round!`;
+  } else if (win) {
+    //if action results in win
+    status = `${win} wins!`;
+    message = `${lostCard}${
+      //^^lists name of won top card^^
+      tieCardsLength > 0 //lists names of won tie cards (if any)
+        ? `,${tieCards.map((card) => {
+            if (tieCards.length - 1 === tieCards.indexOf(card)) {
+              return ` and ${card.name}`;
+            } else {
+              return ` ${card.name}`;
+            }
+          })} `
+        : " "
+    }
+    added to the back of ${win.toLowerCase()}'s deck!`;
+  } else {
+    if (playerTurn) {
+      //if player turn
+      status = "Player's turn";
+      message = "Select a field to play against your opponent:";
+    } else {
+      //if computer turn
+      status = "Computer's turn";
+      message = `Computer plays ${cardHighField}!`;
+    }
+  }
+
+  return (
+    <div className="game-ui">
+      <div className="status">{status}</div>
+      {!win && tieCardsLength > 0 && (
+        <div className="tie">{tieCardsLength} tie cards on the table!</div>
+      )}
+      <div className="message">{message}</div>
+      {gameWinner && (
         <div className="buttons">
           <button onClick={() => window.location.reload()}>Play again</button>
           <Link to="/">
             <button>Back</button>
           </Link>
         </div>
-      </div>
-    );
-  }
-  if (win === "Tie") {
-    return (
-      <div className="game-ui">
-        <div className="status">{win}!</div>
-        <div className="message">
-          Top player and computer cards added to tie deck.
-          <br />
-          <br />
-          {tieCardsLength} tie cards up for grabs next round!
-        </div>
-        <div className="clearAlert">
-          <button className="clearAlert" onClick={clearWin}>
-            OK
-          </button>
-        </div>
-      </div>
-    );
-  }
-  if (win) {
-    return (
-      <div className="game-ui">
-        <div className="win">
-          <div className="status">{win} wins!</div>
-          <div className="message">
-            {lostCard}
-            {tieCardsLength > 0
-              ? `, ${tieCards.map((card) => {
-                  if (tieCards.length - 1 === tieCards.indexOf(card)) {
-                    return ` and ${card.name}`;
-                  } else {
-                    return ` ${card.name}`;
-                  }
-                })} `
-              : " "}
-            added to the back of {win.toLowerCase()}'s deck!
+      )}
+      {win && (
+        <button className="clearAlert" onClick={clearWin}>
+          OK
+        </button>
+      )}
+      {playerTurn && !noCards && !win && !gameWinner && (
+        <div>
+          <div className="field" onClick={() => playField("cool")}>
+            Cool
           </div>
-          <button className="clearAlert" onClick={clearWin}>
-            OK
-          </button>
+          <div className="field" onClick={() => playField("largeness")}>
+            Largeness
+          </div>
+          <div className="field" onClick={() => playField("handsome")}>
+            Handsome
+          </div>
         </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="game-ui">
-        {playerTurn ? (
-          <div className="player-turn">
-            <div className="status">
-              {noCards ? `No cards found!` : `Player's turn`}
-            </div>
-            {tieCardsLength > 0 && (
-              <div className="tie">
-                {tieCardsLength} tie cards on the table!
-              </div>
-            )}
-            <div className="message">
-              {noCards
-                ? `Go to "Create Cards" from the menu to add cards to your deck.`
-                : `Select a field to play against your opponent:`}
-            </div>
-            {!noCards && (
-              <div>
-                <div className="field" onClick={() => playField("cool")}>
-                  Cool
-                </div>
-                <div className="field" onClick={() => playField("largeness")}>
-                  Largeness
-                </div>
-                <div className="field" onClick={() => playField("handsome")}>
-                  Handsome
-                </div>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className="computer-turn">
-            <div className="status">Computer's Turn</div>
-            {tieCardsLength > 0 && (
-              <div className="tie">
-                {tieCardsLength} tie cards on the table!
-              </div>
-            )}
-            <div className="message">Computer plays {cardHighField}!</div>
-            <button
-              className="clearAlert"
-              onClick={() => playField(cardHighField)}
-            >
-              OK
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
+      )}
+      {!playerTurn && !win && (
+        <button className="clearAlert" onClick={() => playField(cardHighField)}>
+          OK
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default GameUI;
