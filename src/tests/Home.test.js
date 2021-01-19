628a72dfd634fcd156b2d647a96b93f5a5c83eeb
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-19 18:27:00 +0000

    wrap tests in describe

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -2,47 +2,49 @@ import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
 
-test("renders correctly", () => {
-  const { asFragment } = render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  expect(asFragment).toMatchSnapshot();
-});
+describe("Home component", () => {
+  test("renders correctly", () => {
+    const { asFragment } = render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(asFragment).toMatchSnapshot();
+  });
 
-test("renders two images", () => {
-  const { getAllByRole } = render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  expect(getAllByRole("img")).toHaveLength(2);
-});
+  test("renders two images", () => {
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(getAllByRole("img")).toHaveLength(2);
+  });
 
-test("renders three buttons", () => {
-  const { getAllByRole } = render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  expect(getAllByRole("button")).toHaveLength(3);
-});
+  test("renders three buttons", () => {
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(getAllByRole("button")).toHaveLength(3);
+  });
 
-test("renders four links", () => {
-  const { getAllByRole } = render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  expect(getAllByRole("link")).toHaveLength(4);
-});
+  test("renders four links", () => {
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(getAllByRole("link")).toHaveLength(4);
+  });
 
-test("renders footer text", () => {
-  const { getByText } = render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  expect(getByText("Created by Tom Hammersley 2021")).toBeInTheDocument();
+  test("renders footer text", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(getByText("Created by Tom Hammersley 2021")).toBeInTheDocument();
+  });
 });
