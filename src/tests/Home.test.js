f2158f9f58c436c100acf47eba5e97c5e11a15ed
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-19 17:33:17 +0000

    update Home component tests

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -11,6 +11,15 @@ test("renders correctly", () => {
   expect(asFragment).toMatchSnapshot();
 });
 
+test("renders two images", () => {
+  const { getAllByRole } = render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  expect(getAllByRole("img")).toHaveLength(2);
+});
+
 test("renders three buttons", () => {
   const { getAllByRole } = render(
     <BrowserRouter>
@@ -18,4 +27,22 @@ test("renders three buttons", () => {
     </BrowserRouter>
   );
   expect(getAllByRole("button")).toHaveLength(3);
+});
+
+test("renders four links", () => {
+  const { getAllByRole } = render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  expect(getAllByRole("link")).toHaveLength(4);
+});
+
+test("renders footer text", () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  expect(getByText("Created by Tom Hammersley")).toBeInTheDocument();
 });
