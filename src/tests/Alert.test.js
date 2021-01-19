e0f3f784c030e0949eae9f4c3c9f29b9b79dcd00
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-19 15:00:44 +0000

    wrap in tests in 'describe' for consistency

--- src/tests/Alert.test.js
+++ src/tests/Alert.test.js
@@ -2,72 +2,74 @@ import { render, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Alert from "../components/Alert";
 
-test("renders correctly", () => {
-  const { asFragment } = render(<Alert />);
-  expect(asFragment).toMatchSnapshot();
-});
+describe("Alert component", () => {
+  test("renders correctly", () => {
+    const { asFragment } = render(<Alert />);
+    expect(asFragment).toMatchSnapshot();
+  });
 
-test("does not render an alert box if message prop is empty", () => {
-  const { queryByText } = render(<Alert alertType="Success" />);
-  expect(queryByText("Success!")).not.toBeInTheDocument();
-});
+  test("does not render an alert box if message prop is empty", () => {
+    const { queryByText } = render(<Alert alertType="Success" />);
+    expect(queryByText("Success!")).not.toBeInTheDocument();
+  });
 
-test("renders an error message and back button", () => {
-  const { getByText } = render(
-    <BrowserRouter>
-      <Alert alertType="Error" message="test" />
-    </BrowserRouter>
-  );
-  expect(getByText("Error")).toBeInTheDocument();
-  expect(getByText("test")).toBeInTheDocument();
-  expect(getByText("BACK")).toBeInTheDocument();
-});
+  test("renders an error message and back button", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Alert alertType="Error" message="test" />
+      </BrowserRouter>
+    );
+    expect(getByText("Error")).toBeInTheDocument();
+    expect(getByText("test")).toBeInTheDocument();
+    expect(getByText("BACK")).toBeInTheDocument();
+  });
 
-test("renders a success message and ok button", () => {
-  const { getByText } = render(
-    <BrowserRouter>
-      <Alert alertType="Success" message="test2" />
-    </BrowserRouter>
-  );
-  expect(getByText("Success")).toBeInTheDocument();
-  expect(getByText("test2")).toBeInTheDocument();
-  expect(getByText("OK")).toBeInTheDocument();
-});
+  test("renders a success message and ok button", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Alert alertType="Success" message="test2" />
+      </BrowserRouter>
+    );
+    expect(getByText("Success")).toBeInTheDocument();
+    expect(getByText("test2")).toBeInTheDocument();
+    expect(getByText("OK")).toBeInTheDocument();
+  });
 
-test("renders a confirmation message and two buttons", () => {
-  const { getByText } = render(
-    <BrowserRouter>
-      <Alert alertType="Are you sure?" message="test3" />
-    </BrowserRouter>
-  );
-  expect(getByText("Are you sure?")).toBeInTheDocument();
-  expect(getByText("test3")).toBeInTheDocument();
-  expect(getByText("CANCEL")).toBeInTheDocument();
-  expect(getByText("DELETE")).toBeInTheDocument();
-});
+  test("renders a confirmation message and two buttons", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Alert alertType="Are you sure?" message="test3" />
+      </BrowserRouter>
+    );
+    expect(getByText("Are you sure?")).toBeInTheDocument();
+    expect(getByText("test3")).toBeInTheDocument();
+    expect(getByText("CANCEL")).toBeInTheDocument();
+    expect(getByText("DELETE")).toBeInTheDocument();
+  });
 
-test("alert clears on OK press", () => {
-  const clearAlert = jest.fn();
-  const { getByText } = render(
-    <BrowserRouter>
-      <Alert alertType="Success" message="test" setAlert={clearAlert} />
-    </BrowserRouter>
-  );
-  fireEvent.click(getByText("OK"));
-  expect(clearAlert).toHaveBeenCalled();
-});
+  test("alert clears on OK press", () => {
+    const clearAlert = jest.fn();
+    const { getByText } = render(
+      <BrowserRouter>
+        <Alert alertType="Success" message="test" setAlert={clearAlert} />
+      </BrowserRouter>
+    );
+    fireEvent.click(getByText("OK"));
+    expect(clearAlert).toHaveBeenCalled();
+  });
 
-test("deletes all cards on confirmation press", () => {
-  const deleteCards = jest.fn();
-  const { getByText } = render(
-    <BrowserRouter>
-      <Alert
-        alertType="Are you sure?"
-        message="test"
-        deleteAllCards={deleteCards}
-      />
-    </BrowserRouter>
-  );
-  fireEvent.click(getByText("DELETE"));
-  expect(deleteCards).toHaveBeenCalled();
+  test("deletes all cards on confirmation press", () => {
+    const deleteCards = jest.fn();
+    const { getByText } = render(
+      <BrowserRouter>
+        <Alert
+          alertType="Are you sure?"
+          message="test"
+          deleteAllCards={deleteCards}
+        />
+      </BrowserRouter>
+    );
+    fireEvent.click(getByText("DELETE"));
+    expect(deleteCards).toHaveBeenCalled();
+  });
 });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,41 +2,43 @@ import { render, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../components/App";
 
-test("renders correctly", () => {
-  const { asFragment } = render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  expect(asFragment).toMatchSnapshot();
-});
+describe("App component", () => {
+  test("renders correctly", () => {
+    const { asFragment } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(asFragment).toMatchSnapshot();
+  });
 
-test("renders the page title", () => {
-  const { getByText } = render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  expect(getByText("Animal Top Trumps")).toBeInTheDocument();
-});
+  test("renders the page title", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(getByText("Animal Top Trumps")).toBeInTheDocument();
+  });
 
-test("renders two home links", () => {
-  const { getAllByRole } = render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  expect(getAllByRole("link")).toBeTruthy();
-  expect(getAllByRole("link")[0]).toHaveAttribute("href", "/");
-  expect(getAllByRole("link")[1]).toHaveAttribute("href", "/");
-});
+  test("renders two home links", () => {
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(getAllByRole("link")).toBeTruthy();
+    expect(getAllByRole("link")[0]).toHaveAttribute("href", "/");
+    expect(getAllByRole("link")[1]).toHaveAttribute("href", "/");
+  });
 
-test("switches components", () => {
-  const { getByText } = render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  fireEvent.click(getByText("Browse Cards"));
-  expect(getByText("Loading...")).toBeTruthy();
+  test("switches components", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    fireEvent.click(getByText("Browse Cards"));
+    expect(getByText("Loading...")).toBeTruthy();
+  });
 });
