449dc67b18fd98754300cec3d8225b250231ea21
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-23 12:12:21 +0000

    restyle Card, install unsplash-api so photo corresponding to Card name is rendered

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,11 @@
 import "../styles/Card.css";
+import { useEffect, useState } from "react";
+import { createApi } from "unsplash-js";
 import logo from "../dog.png";
+
+const api = createApi({
+  accessKey: "jsioAkwwM5Hg_da8KPqPHaVc67Y-HkofLqQY7NEbOL0",
+});
 
 const Card = ({
   name,
@@ -13,11 +19,33 @@ const Card = ({
   hide,
   deckSize,
 }) => {
+  const [photo, setPhoto] = useState(null);
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    setLoad(true);
+    api.search
+      .getPhotos({
+        query: name,
+        orientation: "landscape",
+        page: 1,
+        perPage: 1,
+      })
+      .then((result) => {
+        setPhoto(result.response.results[0]);
+        setLoad(false);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+        setLoad(false);
+      });
+  }, [name]);
+
   if (aka === "default__card") {
     return (
       <div className="Card-border">
         <div className="Card">
-          <div className="other-stat">{name}</div>
+          <div className="name">{name}</div>
         </div>
       </div>
     );
@@ -30,31 +58,83 @@ const Card = ({
           Animal <br />
           Top Trumps
         </div>
-        <img id="card-logo" src={logo} alt="Logo" height="96px" width="96px" />
+        <img
+          id="card-logo-back"
+          src={logo}
+          alt="Logo"
+          height="96px"
+          width="96px"
+        />
       </div>
     );
-  } else {
+  } else if (!load) {
     return (
       <div className="Card-border">
         <div className="Card">
           <div className="name">{name}</div>
-          <div className="other-stat">Cool: {cool} </div>
-          <div className="other-stat">Largeness: {largeness} </div>
-          <div className="other-stat">Handsome: {handsome} </div>
-          <div className="other-stat">
-            aka: <br />"{aka}"{" "}
+          <div className="aka">"{aka}"</div>
+          <div className="alignment">({alignment})</div>
+          {load ? (
+            <div className="cardload">Loading Photo...</div>
+          ) : !deleteCard ? (
+            <img
+              id="card-logo-front"
+              src={photo.urls.small}
+              alt="Logo"
+              height="150px"
+              width="200px"
+            />
+          ) : (
+            <img
+              id="card-logo-front"
+              src={photo.urls.small}
+              alt="Logo"
+              height="100px"
+              width="150px"
+            />
+          )}
+          <div className="stat-name1">
+            Cool: <div className="stat1">{cool}</div>
           </div>
-          <div className="other-stat">
-            Alignment:
-            <br />
-            {alignment}{" "}
+          <div className="stat-name2">
+            Largeness: <div className="stat2">{largeness}</div>
           </div>
+          <div className="stat-name1">
+            Handsome: <div className="stat1">{handsome}</div>
+          </div>
+
           {deleteCard && (
             <div className="delete">
               <button onClick={() => deleteCard(id)}>DELETE</button>
             </div>
           )}
+          <div className="footer">
+            Photo by{" "}
+            <a
+              className="photographer-link"
+              href={`https://unsplash.com/@${photo.user.username}?utm_source=Animal Top Trumps&utm_medium=referral`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {photo.user.name}
+            </a>{" "}
+            on{" "}
+            <a
+              className="photographer-link"
+              href="https://unsplash.com/?utm_source=Animal Top Trumps&utm_medium=referral"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Unsplash
+            </a>
+          </div>
         </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Card-border">
+        <div className="Card"></div>
       </div>
     );
   }
